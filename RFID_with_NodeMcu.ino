503f45d4e5dec095a009f56227bfdd8c80d4377a
Noh Ji-Hye <95845768+omniset@users.noreply.github.com>
2022-12-14 06:17:26 +0700

    Add files via upload
    RFID card reader with ESP 8266

---
+++ RFID_with_NodeMcu.ino
@@ -0,0 +1,56 @@
+#define SS_PIN 4  //D2
+#define RST_PIN 5 //D1
+#define LED_R 2 //define red LED
+#include <SPI.h>
+#include <MFRC522.h>
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance/object.
+int variable = 0;
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  pinMode(LED_R, OUTPUT);
+  Serial.println("Show your card:");
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print(" UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  Serial.println();
+  if (content.substring(1) == "C1 9A 15 44") //change UID of the card that you want to give access
+  {
+    Serial.println(" Authorized Access ");
+    Serial.println(" Welcome Mandalan ");
+    delay(500);
+    digitalWrite(LED_R, HIGH);
+    delay(5000);
+    digitalWrite(LED_R, LOW);
+  }
+  
+  else   {
+    Serial.println(" Access Denied ");
+    delay(3000);
+  }
+} 
